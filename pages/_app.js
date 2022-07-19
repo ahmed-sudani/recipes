70cfaa5ba32033727b67afe890ac8784e70a8f7a
MusabKamel <masabkamel2000@gmail.com>
2022-07-19 09:47:05 +0200

    add footer and nav in app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
