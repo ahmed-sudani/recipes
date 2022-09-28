6345bbd727f93673edcfaeb4c761e400dbaaf0bc
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-09-28 11:05:29 +0200

    add head tag to developers page

--- pages/developers/index.js
+++ pages/developers/index.js
@@ -1,24 +1,28 @@
+import Head from '../../components/head'
 import Person from '../../components/person'
 import styles from './styles.module.css'
 export default function Developers() {
   return (
-    <div className={styles.container}>
-      <Person
-        img="/mosab.png"
-        name="Musab Kamil"
-        jobTitle="Front-End Developer"
-        description="Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+    <>
+      <Head title="Developers" description="Developers of recipes site" />
+      <div className={styles.container}>
+        <Person
+          img="/mosab.png"
+          name="Musab Kamil"
+          jobTitle="Front-End Developer"
+          description="Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
                   Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in,
                   elementum id enim."
-      ></Person>
-      <Person
-        img="/ahmed.png"
-        name="Ahmed Abdalla"
-        jobTitle="Software Developer"
-        description="Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+        ></Person>
+        <Person
+          img="/ahmed.png"
+          name="Ahmed Abdalla"
+          jobTitle="Software Developer"
+          description="Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
                   Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in,
                   elementum id enim."
-      ></Person>
-    </div>
+        ></Person>
+      </div>
+    </>
   )
 }
