b4e553782c3783c272f349f8e64405ac94e9c19e
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-09-30 23:15:18 +0200

    test button rendering by snapshot

--- components/button/__test__/button.test.js
+++ components/button/__test__/button.test.js
@@ -1,11 +1,11 @@
-import { render, screen, within } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Button from '..'
 
 describe('Button', () => {
   it('should render button component', () => {
     render(<Button text="test" />)
     const buttonElement = screen.getByRole('button')
-    expect(buttonElement).toBeDefined()
+    expect(buttonElement).toMatchSnapshot()
   })
   it('should match the text passed to button', () => {
     render(<Button text="test" />)
