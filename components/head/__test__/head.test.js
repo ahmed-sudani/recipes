0c4265a3368c2b76a057192e3b3bf296c18cecf4
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-09-30 23:17:57 +0200

    test head rendering by snapshot

--- components/head/__test__/head.test.js
+++ components/head/__test__/head.test.js
@@ -1,16 +1,21 @@
-import { render, screen, waitFor, within } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Head from '..'
 
 jest.mock('next/head', () => {
   return {
     __esModule: true,
     default: ({ children }) => {
-      return <>{children}</>
+      return <div data-testid="head">{children}</div>
     },
   }
 })
 
 describe('Head', () => {
+  it('should render correctly', async () => {
+    render(<Head title={'test'} description="this is test" />)
+    const headElement = screen.getByTestId(/head/i)
+    expect(headElement).toMatchSnapshot()
+  })
   it('title should match the text passed', async () => {
     render(<Head title={'test'} description="this is test" />)
     expect(document.title).toEqual('test')
