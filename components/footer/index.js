c81a3f4ff8e9ce336a2b44ef5f78a19725196961
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-09-30 21:20:33 +0200

    test footer component

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,4 +1,4 @@
 import styles from './styles.module.css'
 export default function Footer() {
-  return <footer className={styles.footer}> all copy right resaved</footer>
+  return <footer className={styles.footer}> all copy right reserved</footer>
 }
