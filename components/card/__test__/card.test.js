951bbe1fcc84037dcf3543ea7645da497d33ec65
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-09-30 23:16:26 +0200

    test card rendering by snapshot

--- components/card/__test__/card.test.js
+++ components/card/__test__/card.test.js
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Card from '..'
 
 const card = {
@@ -11,31 +11,6 @@ describe('Card', () => {
   it('should render card component', () => {
     render(<Card {...card} />)
     const cardElement = screen.getByTestId('card')
-    expect(cardElement).toBeDefined()
-  })
-
-  it('card name should match the passed text', () => {
-    render(<Card {...card} />)
-    const cardElement = screen.getByTestId('card')
-    const nameElement = within(cardElement).getByText(card.name)
-    expect(nameElement).toBeDefined()
-  })
-  it('card time should match the passed text', () => {
-    render(<Card {...card} />)
-    const cardElement = screen.getByTestId('card')
-    const timeElement = within(cardElement).getByText(card.time)
-    expect(timeElement).toBeDefined()
-  })
-  it('card country should match the passed text', () => {
-    render(<Card {...card} />)
-    const cardElement = screen.getByTestId('card')
-    const timeElement = within(cardElement).getByText(card.country)
-    expect(timeElement).toBeDefined()
-  })
-  it('image element should be defined', () => {
-    render(<Card {...card} />)
-    const cardElement = screen.getByTestId('card')
-    const imageElement = within(cardElement).getByAltText(card.name)
-    expect(imageElement).toBeDefined()
+    expect(cardElement).toMatchSnapshot()
   })
 })
