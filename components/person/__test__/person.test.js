28c4a17d79b871ef92de1046e89cc46f8d636dc1
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-09-30 23:18:20 +0200

    test person rendering by snapshot

--- components/person/__test__/person.test.js
+++ components/person/__test__/person.test.js
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Person from '..'
 
 const person = {
@@ -8,35 +8,9 @@ const person = {
   description: 'good software developer',
 }
 describe('Person', () => {
-  it('should render person component', () => {
+  it('should render person component correctly', () => {
     render(<Person {...person} />)
     const personElement = screen.getByTestId('person')
-    expect(personElement).toBeDefined()
-  })
-  it('name element should be defined', () => {
-    render(<Person {...person} />)
-    const personElement = screen.getByTestId('person')
-    const nameElement = within(personElement).getByText(person.name)
-    expect(nameElement).toBeDefined()
-  })
-  it('image element should be defined', () => {
-    render(<Person {...person} />)
-    const personElement = screen.getByTestId('person')
-    const imageElement = within(personElement).getByAltText(person.name)
-    expect(imageElement).toBeDefined()
-  })
-  it('jobTitle element should be defined', () => {
-    render(<Person {...person} />)
-    const personElement = screen.getByTestId('person')
-    const jobTitleElement = within(personElement).getByText(person.jobTitle)
-    expect(jobTitleElement).toBeDefined()
-  })
-  it('description element should be defined', () => {
-    render(<Person {...person} />)
-    const personElement = screen.getByTestId('person')
-    const descriptionElement = within(personElement).getByText(
-      person.description
-    )
-    expect(descriptionElement).toBeDefined()
+    expect(personElement).toMatchSnapshot()
   })
 })
