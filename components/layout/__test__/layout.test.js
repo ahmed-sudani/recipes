2151e098e3e154d8745816a506ef974cd2c07f83
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-10-01 09:55:39 +0200

    mocking layout and useSession in layout test

--- components/layout/__test__/layout.test.js
+++ components/layout/__test__/layout.test.js
@@ -1,14 +1,22 @@
 import { render, screen } from '@testing-library/react'
+import { useSession } from 'next-auth/react'
 import Layout from '..'
 
+jest.mock('next-auth/react')
+
+const MockedLayout = () => (
+  <span data-testid="layout">
+    <Layout />
+  </span>
+)
+
 describe('Layout', () => {
-  it('should render layout component', () => {
-    render(
-      <span data-testid="layout">
-        <Layout />
-      </span>
-    )
-    const layoutElement = screen.getByTestId(/layout/i)
+  it('should renders correctly', () => {
+    useSession.mockReturnValue({
+      data: null,
+    })
+    render(<MockedLayout />)
+    const layoutElement = screen.getByTestId('layout')
     expect(layoutElement).toMatchSnapshot()
   })
 })
