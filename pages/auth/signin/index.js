65b9a8d6c3cfc5731cf88f1033123808daba8766
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-10-03 23:39:46 +0200

    update text

--- pages/auth/signin/index.js
+++ pages/auth/signin/index.js
@@ -1,18 +1,22 @@
 import { getCsrfToken } from 'next-auth/react'
 import Button from '../../../components/button'
 import Head from '../../../components/head'
-
 import styles from './styles.module.css'
 export default function Signin({ csrfToken }) {
   return (
     <>
-      <Head title="Login" description="Login To Save Your Favourites Recipes" />
+      <Head
+        title="Login"
+        description="Login To Start Add and Save Your Favorites Recipes"
+      />
       <form
         className={styles.Form}
         action="/api/auth/signin/email"
         method="POST"
       >
-        <div className={styles.Text}>Login To Save Your Favourites Recipes</div>
+        <div className={styles.Text}>
+          Login To Start Add and Save Your Favorites Recipes
+        </div>
         <div className={styles.inputForm}>
           <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
           <label> Email </label>
