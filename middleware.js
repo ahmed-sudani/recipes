e073957d363fb56e78a2858132ca5eec52e01c8f
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-10-03 23:29:22 +0200

    auth middleware

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+const secret = process.env.NEXT_AUTH_SECRET
+
+/**
+ * @param {import('next').NextApiRequest} req
+ */
+
+export async function middleware(req) {
+  const token = await getToken({ req, secret })
+  if (!token) {
+    req.nextUrl.searchParams.set('callbackUrl', req.url)
+    req.nextUrl.pathname = '/auth/signin'
+    return NextResponse.redirect(req.nextUrl)
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/favorites', '/new'],
+}
