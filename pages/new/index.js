b4d1367c9576938f2d669d42cbcc066a719f68f8
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-10-04 20:15:08 +0200

    create new recipes required function

--- pages/new/index.js
+++ pages/new/index.js
@@ -1,83 +1,157 @@
+import axios from 'axios'
 import Image from 'next/image'
 import { useRef, useState } from 'react'
+import { EggFried, PlusSquare } from 'react-bootstrap-icons'
 import Button from '../../components/button'
 import Head from '../../components/head'
 import InputWithLabel from '../../components/InputWithLabel'
-import { EggFried, PlusSquare } from 'react-bootstrap-icons'
 import styles from './styles.module.css'
-import { isAbsoluteUrl } from 'next/dist/shared/lib/utils'
+import { useRouter } from 'next/router'
+
 export default function AddRecipe() {
+  const router = useRouter()
+  const [name, setName] = useState('')
   const [image, setImage] = useState('')
-  const [ingredients, setIngredients] = useState([''])
-  const inputRef = useRef()
+  const [country, setCountry] = useState('')
+  const [time, setTime] = useState('')
+  const [ingredients, setIngredients] = useState([])
+  const ingredientsInputRef = useRef()
+
+  const addIngredients = () => {
+    if (ingredientsInputRef.current.value)
+      setIngredients([...ingredients, ingredientsInputRef.current.value])
+    ingredientsInputRef.current.value = ''
+  }
+  const IngredientsList = () => {
+    return ingredients.map((ingredient, index) => (
+      <li
+        onClick={() => {
+          setIngredients((prev) => prev.filter((value) => value != ingredient))
+        }}
+        key={index}
+      >
+        {ingredient}
+      </li>
+    ))
+  }
+
+  const updateInput = (setValue) => {
+    return (e) => {
+      setValue(e.target.value)
+    }
+  }
+
+  const updateImageInput = (setValue) => {
+    return (e) => {
+      try {
+        if (new URL(e.target.value).protocol === 'https:') {
+          setValue(e.target.value)
+        }
+      } catch (error) {
+        setImage('')
+      }
+    }
+  }
+
+  const ImageOnError = (e) => {
+    setImage('')
+  }
+  const ImageOrIcon = () => {
+    return (
+      <div className={styles.image}>
+        {image && (
+          <Image
+            src={`https://res.cloudinary.com/demo/image/fetch/${image}`}
+            height={300}
+            width={300}
+            alt={image}
+            onError={ImageOnError}
+            objectFit="cover"
+          />
+        )}
+        {!image && <EggFried size={200} />}
+      </div>
+    )
+  }
+
+  const sendApiRequest = async () => {
+    let res = await axios.post(
+      '/api/recipes',
+      {
+        name,
+        country,
+        time,
+        image,
+        ingredients,
+      },
+      { withCredentials: true }
+    )
+    if (res.status == 201) {
+      router.push('/recipes')
+    }
+  }
+
   return (
     <>
-      <Head title="Create new recipe" description="Create new recipe" />
-      <div className={styles.container}>
-        <div className={styles.image}>
-          {image && (
-            <Image
-              src={`https://res.cloudinary.com/demo/image/fetch/${image}`}
-              height={300}
-              width={300}
-              alt={image}
-              onError={(e) => {
-                setImage('')
-              }}
-              objectFit="cover"
-            />
-          )}
-          {!image && <EggFried size={200} />}
-        </div>
+      <Head title="Create New Recipe" description="create new recipe" />
 
-        <form className={styles.column} action="/api/recipes" method="POST">
+      <div className={styles.container}>
+        <ImageOrIcon />
+
+        <form
+          className={styles.column}
+          action="/api/recipes"
+          method="POST"
+          onSubmit={(e) => {
+            e.preventDefault()
+            sendApiRequest()
+          }}
+        >
           <div className={styles.Text}>Add new recipe to the site</div>
-          <InputWithLabel name="Name" type="text" />
+          <InputWithLabel
+            name="Name"
+            minLength={5}
+            maxLength={50}
+            type="text"
+            onBlur={updateInput(setName)}
+          />
           <InputWithLabel
             name="Image Url"
             type="url"
-            onBlur={(e) => {
-              try {
-                if (new URL(e.target.value).protocol === 'https:') {
-                  e.target.value
-                }
-                setImage(e.target.value)
-              } catch (error) {
-                setImage('')
-              }
-            }}
+            maxLength={100}
+            onBlur={updateImageInput(setImage)}
           />
-          <InputWithLabel name="Country" type="text" />
+          <InputWithLabel
+            name="Country"
+            minLength={2}
+            maxLength={50}
+            type="text"
+            onBlur={updateInput(setCountry)}
+          />
           <InputWithLabel
             name="Required Time"
             min={1}
             max={180}
             type="number"
+            onBlur={updateInput(setTime)}
           />
+
           <div className={styles.inputForm}>
-            <label htmlFor="">Ingredients</label>
+            <label>Ingredients</label>
             <div className={styles.ingredientsInput}>
-              <input type="text" ref={inputRef} />
-              <button
-                type="button"
-                onClick={() => {
-                  if (inputRef.current.value)
-                    setIngredients([...ingredients, inputRef.current.value])
-                  inputRef.current.value = ''
-                }}
-              >
+              <input type="text" ref={ingredientsInputRef} />
+              <button type="button" onClick={addIngredients}>
                 <PlusSquare size={30} />
               </button>
             </div>
           </div>
 
-          <Button text="Create Recipe" />
+          <Button text="Create Recipe" type="submit" />
         </form>
-        <div className={styles.column}>
-          {ingredients.map((ingredient, index) => (
-            <div key={index}>{ingredient}</div>
-          ))}
-        </div>
+
+        <ul className={styles.list}>
+          <IngredientsList />
+        </ul>
       </div>
     </>
   )
