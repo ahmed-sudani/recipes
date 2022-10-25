cc1c3b4eead3b2e15ce07782b25243b0fc31741f
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-10-25 09:37:54 +0200

    change cards to recipes

--- src/common/components/lists/recipesList/index.js
+++ src/common/components/lists/recipesList/index.js
@@ -1,7 +1,9 @@
 import { Recipe } from '../../cards'
 import styles from './styles.module.css'
 export function RecipesList(props) {
-  let cardsComp = props.cards.map((item) => <Recipe key={item._id} {...item} />)
+  let cardsComp = props.recipes.map((item) => (
+    <Recipe key={item._id} {...item} />
+  ))
   return (
     <div className={styles.container} data-testid="container">
       {cardsComp}
